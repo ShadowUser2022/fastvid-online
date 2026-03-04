cbd6ea5a8f835806358c5bec57a57cbc78611208
ShadowUser2022 <ShadowUser2022@users.noreply.github.com>
2026-03-04 20:33:26 +0200

    fix: ensure canonical www redirect for ads.txt bot

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	output: 'standalone',
 
+	async redirects() {
+		return [
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'fastvid.online',
+					},
+				],
+				destination: 'https://www.fastvid.online/:path*',
+				permanent: true,
+			},
+		];
+	},
+
 	async headers() {
 		return [
 			{
